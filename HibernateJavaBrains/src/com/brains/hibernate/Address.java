0130241123f7985b9cbd95bc0d88b5b0aeb5bdb3
PLM-AHMEDW\Admin <ahmed@pragiti.com>
2019-06-26 12:11:52 +0530

    add

---
+++ HibernateJavaBrains/src/com/brains/hibernate/Address.java
@@ -0,0 +1,31 @@
+package com.brains.hibernate;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+	private String city;
+	private String State;
+	private String pincode;
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	
+	
+}
