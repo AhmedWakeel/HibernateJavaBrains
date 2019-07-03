8d2778fa25a6ca8033ce50b7264e2ef894c70294
PLM-AHMEDW\Admin <ahmed@pragiti.com>
2019-07-03 15:24:05 +0530

    one-To-one

--- HibernateJavaBrains/src/com/brains/hibernate/Main.java
+++ HibernateJavaBrains/src/com/brains/hibernate/Main.java
@@ -1,8 +1,5 @@
 package com.brains.hibernate;
 
-import java.util.Date;
-
-import javax.persistence.FetchType;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
 import javax.transaction.RollbackException;
@@ -10,7 +7,6 @@ import javax.transaction.SystemException;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
@@ -20,37 +16,21 @@ public class Main {
 	
 		UserDetails details = new UserDetails();
 		details.setUserName("Malik");
-		details.setAddress("mumbai");
-		details.setDescription("java");
-		details.setJoinedDate(new Date());
 		
+		Vehicle vehicle = new Vehicle();
+		vehicle.setVehicleName("TATA");
 		
-		Address  address = new Address();
-		address.setCity("mumbai");
-		address.setPincode("400072");
-		address.setState("maharashtraa");
-		
-		Address  address2 = new Address();
-		address2.setCity("mumbai1");
-		address2.setPincode("4000721");
-		address2.setState("maharashtraa11");
-		
-		details.getAddresses().add(address);
-		details.getAddresses().add(address2);
-		
+       details.setVehicle(vehicle);		
 		
 		SessionFactory buildSessionFactory = new Configuration().configure().buildSessionFactory();
-		Session openSession = buildSessionFactory.openSession();
-		openSession.beginTransaction();
+		Session session = buildSessionFactory.openSession();
+		session.beginTransaction();
 		
-		openSession.save(details);
-		openSession.getTransaction().commit();
-		openSession.close();
+		session.save(details);
+		session.save(vehicle);
+		session.getTransaction().commit();
+		session.close();
 		 
-		openSession = buildSessionFactory.openSession();
-		UserDetails userDetails1 = openSession.get(UserDetails.class, 1);
-		openSession.close(); // if we close the session and removes (fetch=FetchType.EAGER) from UserDetails.java then we will get LazyInitialization exception
-		System.out.println(userDetails1.getAddresses().size());
-		
+	
 	}
 }
--- HibernateJavaBrains/src/com/brains/hibernate/UserDetails.java
+++ HibernateJavaBrains/src/com/brains/hibernate/UserDetails.java
@@ -1,32 +1,13 @@
 package com.brains.hibernate;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.Lob;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Transient;
-
-import org.hibernate.annotations.CollectionId;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name = "UserDetails")
@@ -36,45 +17,19 @@ public class UserDetails {
 //	@GeneratedValue // this annotation is used to add values automatically to the primary key column
 	@GeneratedValue(strategy =GenerationType.AUTO)
 	private int userId;
-	@Transient    // if we make any field transient then hibernate will igonre the that field and will not create the column in the table.
+//	@Transient    // if we make any field transient then hibernate will igonre the that field and will not create the column in the table.
 	private String userName;
-	@Temporal(TemporalType.DATE)  // save only date and not time
-	private Date joinedDate;
-	@Lob   // by default hibernate uses 255 characters but if want to save more than that then we can use Large object , BLOB binary large object , CLOB character large object
-	private String Address;
-	private String Description;
+	
+	@OneToOne
+	@JoinColumn(name = "vehicle_ID")
+    private Vehicle vehicle;
+	
     
-	@ElementCollection(fetch=FetchType.EAGER)
-	@JoinTable(name = "ADDRESS" , joinColumns = @JoinColumn(name = "USER_ID"))
-	Collection<Address> addresses = new ArrayList<>();
-	
-	
-	public Collection<Address> getAddresses() {
-		return addresses;
+	public Vehicle getVehicle() {
+		return vehicle;
 	}
-	public void setAddresses(Collection<Address> addresses) {
-		this.addresses = addresses;
-	}
-	public void setAddresses(Set<Address> addresses) {
-		this.addresses = addresses;
-	}
-	public String getAddress() {
-		return Address;
-	}
-	public void setAddress(String address) {
-		Address = address;
-	}
-	public String getDescription() {
-		return Description;
-	}
-	public void setDescription(String description) {
-		Description = description;
-	}
-	public Date getJoinedDate() {
-		return joinedDate;
-	}
-	public void setJoinedDate(Date joinedDate) {
-		this.joinedDate = joinedDate;
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
 	}
 	public int getUserId() {
 		return userId;
