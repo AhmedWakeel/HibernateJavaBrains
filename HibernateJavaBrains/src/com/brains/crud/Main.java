e9a6f1870917fb6c97dedde792bd0669f9c6c9ea
PLM-AHMEDW\Admin <ahmed@pragiti.com>
2019-07-15 13:36:54 +0530

    add

--- HibernateJavaBrains/src/com/brains/crud/Main.java
+++ HibernateJavaBrains/src/com/brains/crud/Main.java
@@ -1,18 +1,13 @@
 package com.brains.crud;
 
-import java.util.List;
-
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
 
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.criterion.Example;
-import org.hibernate.criterion.Order;
 
 public class Main {
 
@@ -23,22 +18,17 @@ public class Main {
 		Session session = buildSessionFactory.openSession();
 		session.beginTransaction();
 	
+		UserDetails userDetails = session.get(UserDetails.class, 1);
+		userDetails.setUserName("mk");
 		
-		UserDetails details = new UserDetails();
-		details.setUserName("Name is%");
-		
-		Example example = Example.create(details).enableLike();  // excludeProperty("UserId").
-		
-		 Criteria add = session.createCriteria(UserDetails.class)
-				                          .addOrder(Order.asc("UserId"))
-				                          .add(example);
-//				                          .setProjection(Projections.max("UserId")); 
-//		                                  .setProjection(Projections.count("UserId"));
-//				                          .setProjection(Projections.property("UserId"));
-		List<UserDetails> list = (List<UserDetails>) add.list();
+		UserDetails userDetails1 = session.get(UserDetails.class, 1);
 		session.getTransaction().commit();
 		session.close(); 
 		
-	
+
+		 session = buildSessionFactory.openSession();
+		session.beginTransaction();
+		
+		UserDetails userDetails2 = session.get(UserDetails.class, 1);
 	}
 }
