999a66445f5f7838f8f53d81b4f0b368f1cd1c12
WakeelMalik <wakeelmalik123@gmail.com>
2019-09-11 22:11:14 +0530

    no message

--- HibernateJavaBrains/src/com/brains/hibernate/Main.java
+++ HibernateJavaBrains/src/com/brains/hibernate/Main.java
@@ -1,5 +1,7 @@
 package com.brains.hibernate;
 
+
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -12,14 +14,16 @@ public class Main {
 		UserDetails details = new UserDetails();
 		details.setUserName("Malik");
 		
-		SessionFactory buildSessionFactory = new Configuration().configure().buildSessionFactory();
+		SessionFactory buildSessionFactory =  new Configuration().configure().buildSessionFactory();
 		Session openSession = buildSessionFactory.openSession();
 		openSession.beginTransaction();
 		
+		 
 		openSession.save(details);
 		Transaction transaction = openSession.getTransaction();
 		transaction.commit();
 		openSession.close();
+		
 		
 	}
 }
